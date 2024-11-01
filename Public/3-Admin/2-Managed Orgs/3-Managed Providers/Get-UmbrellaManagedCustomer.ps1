ef1d21b3385663df114521f266d7f4babf45f968
Michael Arufe <marufe@compassmsp.com>
2024-11-01 10:29:19 -0400

    Added name-based searching of customers

--- Public/3-Admin/2-Managed Orgs/3-Managed Providers/Get-UmbrellaManagedCustomer.ps1
+++ Public/3-Admin/2-Managed Orgs/3-Managed Providers/Get-UmbrellaManagedCustomer.ps1
@@ -3,24 +3,50 @@ function Get-UmbrellaManagedCustomer {
     
     [CmdletBinding()]
     param (
-        [string]$Id
+        [string]$Id,
+        [string]$Name
     )
     
     $temp_token = ConvertFrom-SecureStringToPlainText $script:token
-    $headers = @{"Authorization"="Bearer $temp_token"}
+    $headers = @{"Authorization" = "Bearer $temp_token" }
 
     $Params = @{
-        Method = "GET"
+        Method  = "GET"
         Headers = $headers
     }
 
-    if($Id) {
-        $Params.Add("Uri", $UmbrellaAPIPaths.Admin.ManagedProviderCustomersUrl + "/$Id")
-    } else {
+    if ($Id -or $Name) {
+        if ($Name) {
+            $Params.Add("Uri", $UmbrellaAPIPaths.Admin.ManagedProviderCustomersUrl)
+            $temp_customers = Invoke-RestMethod @Params
+            $search_results = $temp_customers | Where-Object { $_.customerName -match $Name }
+            $results_count = $search_results | Measure-Object
+            #Write-Host $results_count
+            if ($results_count.Count -eq 1) {
+                $search_results
+            }
+            else {
+                Write-Host "Your search returned multiple results. See the results below:"
+                $customers = $search_results
+                $customers | Out-Default
+                Write-Host "Run this cmdlet again, but with the -Id parameter and the ID number of the customer instead of -Name, or use a more specific search string."
+            }
+        }
+        if ($Id) {
+            $Params.Add("Uri", $UmbrellaAPIPaths.Admin.ManagedProviderCustomersUrl + "/$Id")
+
+            $customers = Invoke-RestMethod @Params
+
+            $customers
+        }
+    }
+    else {
         $Params.Add("Uri", $UmbrellaAPIPaths.Admin.ManagedProviderCustomersUrl)
+
+        $customers = Invoke-RestMethod @Params
+
+        $customers
     }
 
-    $customers = Invoke-RestMethod @Params
-
-    $customers
+    
 }
--- Tests/Basics.tests.ps1
+++ Tests/Basics.tests.ps1
@@ -1,7 +1,16 @@
-#Import the module
-Import-Module -Name C:\Users\marufe\GitHub\PSUmbrella -Verbose
+$creds = Get-Credential
+
 Remove-Module PSUmbrella
-
-#Connect to API
-Connect-Umbrella
-
+Import-Module -Name C:\Users\marufe\GitHub\PSUmbrella -Verbose
+Connect-Umbrella $creds
+Get-UmbrellaManagedCustomer -Name "LLC"
+#troubleshoot SINGLE customer search results by name
+$temp_customers = Get-UmbrellaManagedCustomer
+$search_results = $temp_customers | Where-Object {$_.customerName -match "Irwin"}
+$search_results
+#troubleshoot MULTIPLE customer search results by name
+$temp_customers = Get-UmbrellaManagedCustomer
+$search_results = $temp_customers | Where-Object {$_.customerName -match "LLC"}
+$search_results | Measure-Object
+$results_count = $search_results | Measure-Object
+$results_count
