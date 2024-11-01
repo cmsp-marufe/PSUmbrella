99628fe58c89c778197ab3042339163222bb12da
Michael Arufe <66434300+cmsp-marufe@users.noreply.github.com>
2024-10-31 22:52:04 -0400

    Update Disconnect-Umbrella.ps1
    Fixed token variable name

--- Public/1-Auth/Disconnect-Umbrella.ps1
+++ Public/1-Auth/Disconnect-Umbrella.ps1
@@ -1,4 +1,4 @@
 function Disconnect-Umbrella {
-    Remove-Variable -Name Token -Scope Script -Force -Confirm:$false -ErrorAction Stop
+    Remove-Variable -Name token -Scope Script -Force -Confirm:$false -ErrorAction Stop
     Write-Host "Umbrella API disconnected"
 }
