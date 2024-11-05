2ff0140f5e6268869265bfdd48e01f8e3cf5380c
Michael Arufe <marufe@compassmsp.com>
2024-11-05 09:59:05 -0500

    Created a "queue" folder to hold items that require work to get functional, but are not actively being worked on at this time. Updated PSM to reflect this.

--- PSUmbrella.psm1
+++ PSUmbrella.psm1
@@ -1,10 +1,10 @@
 #Get script files
 $Public = @(Get-ChildItem -Path "$PSScriptRoot\Public\" -Filter "*.ps1" -Recurse -ErrorAction SilentlyContinue)
 $Private = @(Get-ChildItem -Path "$PSScriptRoot\Private\" -Filter "*.ps1" -Recurse -ErrorAction SilentlyContinue)
-
+$Dev = @(Get-ChildItem -Path "$PSScriptRoot\Dev\" -Filter "*.ps1" -Recurse -ErrorAction SilentlyContinue)
 #Dot source script files
 
-ForEach ($file in @($Public + $Private)) {
+ForEach ($file in @($Public + $Private + $Dev)) {
     Try {
         . $file.fullname
     }
